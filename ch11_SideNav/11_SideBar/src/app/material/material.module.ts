73a151d69c95c1f1f5aa7ad78e9e4486d354bcc8
peterhchen <peter.hp.chen@gmail.com>
2019-05-21 15:58:50 -0700

    SideNav Open and Close

---
+++ ch11_SideNav/11_SideBar/src/app/material/material.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { 
+  MatButtonModule, 
+  MatButtonToggleModule, 
+  MatIconModule,
+  MatProgressSpinnerModule,
+  MatToolbarModule,
+  MatSidenavModule
+} from '@angular/material';
+import { MatBadgeModule } from '@angular/material/badge';
+
+const material = [
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatIconModule,
+  MatBadgeModule,
+  MatProgressSpinnerModule,
+  MatToolbarModule,
+  MatSidenavModule
+];
+
+@NgModule({
+  imports: [material],
+  exports: [material]
+})
+export class MaterialModule { }
